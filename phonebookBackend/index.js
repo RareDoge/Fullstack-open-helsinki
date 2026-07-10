da6e94e46c2d9478e8cfdcf90c5b279ed8eb5575
RareDoge <corey5564@gmail.com>
2026-07-10 16:39:26 -0700

    update

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -101,7 +101,7 @@ app.head('/users', (request, response) => {
   response.status(200).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
