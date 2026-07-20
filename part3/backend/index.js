0569e2ed41a1edbe42cb4175f2a637cfa835ad33
RareDoge <corey5564@gmail.com>
2026-07-20 10:40:50 -0700

    more error handling and schema regulations in mongoose

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -46,7 +46,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if(!body.content) {
@@ -60,9 +60,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
   })
+  .catch(error => next(error))
 
 })
 
@@ -104,7 +106,9 @@ const errorHandler = (error, request, response, next) =>{
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
